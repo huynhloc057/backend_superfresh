89bb056431fcac6d1296c5d83a553e9a828a8e42
huynhloc057 <huynhtanloc572001@gmail.com>
2022-10-13 21:07:01 +0700

    config complete vercel

--- src/index.js
+++ src/index.js
@@ -20,11 +20,18 @@ mongoose
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 require("./models");
 app.use("/api", routes);
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "public") });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 });
+
+module.exports = app;
